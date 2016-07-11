257f36d7abaa9f38a117b21851f07f56114aa8dd
Luis Pablo <luispablo.gallo@gmail.com>
2016-07-10 23:27:13 -0300

    Remoe ES6 features

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const React = require("react"); // eslint-disable-line no-unused-vars, no-undef
+var React = require("react"); // eslint-disable-line no-unused-vars, no-undef
 
-const DEFAULT_WIDTH = "100%";
-const DEFAULT_MESSAGE = "";
+var DEFAULT_WIDTH = "100%";
+var DEFAULT_MESSAGE = "";
 
-const ProgressBar = (props) => {
-	const style = { width: props.width || DEFAULT_WIDTH };
-	const className = "progress-bar progress-bar-info progress-bar-striped active";
-	const span = React.createElement("span", null, props.message || DEFAULT_MESSAGE);
-	const innerDiv = React.createElement("div", { className, style }, span);
+var ProgressBar = function (props) {
+	var style = { width: props.width || DEFAULT_WIDTH };
+	var className = "progress-bar progress-bar-info progress-bar-striped active";
+	var span = React.createElement("span", null, props.message || DEFAULT_MESSAGE);
+	var innerDiv = React.createElement("div", { className, style }, span);
 
 	return React.createElement("div", { className: "progress" }, innerDiv);
 };
